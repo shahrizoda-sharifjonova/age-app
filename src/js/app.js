4dc2e41284fafd062b76ee408d7516e7224cdeed
shahrizoda <shahrizodadeveloping@gmail.com>
2022-06-25 19:35:20 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,21 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelectorAll('.menu-btn');
+const menuClose = document.querySelector('.menu__close');
+const headerList = document.querySelector('.header__list');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    menu.classList.toggle('active')
+    menuBtn.forEach(el => {
+        el.classList.remove('active')
+        el.classList.toggle('hidden')
+    });
+    menuClose.classList.toggle('active')
+    headerList.classList.toggle('active')
+    body.classList.toggle('hidden')
+})
+
